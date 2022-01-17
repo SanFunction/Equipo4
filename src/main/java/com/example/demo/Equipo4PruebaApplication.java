30cbe807a1c1536fe99bcd8592ea25971657c1ae
Luis <curso@curso-VirtualBox>
2022-01-17 12:49:18 +0100

    Commit1

---
+++ src/main/java/com/example/demo/Equipo4PruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Equipo4PruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Equipo4PruebaApplication.class, args);
+	}
+
+}
